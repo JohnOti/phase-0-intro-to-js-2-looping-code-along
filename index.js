cafc67fe2f9961e6c5b73e28c27b7f1c1ace59e2
John Oduor Otieno <johnoduor222@gmail.com>
2022-08-04 03:00:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+let message = []
+function writeCards(cards, event) {
+  for (let i = 0; i < cards.length; i++) {
+    message.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+  
+    
+   
+  }
+  
+
+  return message;
+}
+
+
+console.log(message);
+
+function countDown(number){
+    while(number>=0){
+        console.log(number--)
+    }
+    return number;
+}
